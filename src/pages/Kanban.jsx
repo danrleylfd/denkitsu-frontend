90062cf61e30b342af82a1159f6115c723c25d40
danrleylfd <danrley.reply+github@gmail.com>
2025-06-12 23:28:53 -0300

    funcione

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -6,7 +6,7 @@ const ContentView = ({ children }) => (
   <main className="flex flex-col items-center px-2 gap-2 mx-auto w-full min-h-screen xs:max-w-[100%] sm:max-w-[90%] md:max-w-[75%] lg:max-w-[85%] ml-[3.5rem] md:ml-auto">{children}</main>
 )
 
-const KanbanPage = () => {
+const Kanban = () => {
   return (
     <SideMenu fixed ContentView={ContentView} className="bg-cover bg-[url('/background.jpg')] bg-brand-purple">
       <KanbanProvider>
@@ -16,4 +16,4 @@ const KanbanPage = () => {
   )
 }
 
-export default KanbanPage
+export default Kanban
