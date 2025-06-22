4770cfb713be5360338aedeb4bc493da083a4083
danrleylfd <danrley.reply+github@gmail.com>
2025-06-22 14:26:01 -0300

    fix(Lousa): ajustar largura e altura do componente Lousa
    - Modificado o estilo do componente Lousa para ocupar 100% da largura e altura disponíveis, melhorando a responsividade do modal.

--- src/components/Lousa.jsx
+++ src/components/Lousa.jsx
@@ -5,7 +5,7 @@ const Lousa = ({ htmlContent, onClose }) => {
   if (!htmlContent) return null
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 backdrop-blur-sm">
-      <div className="relative flex flex-col rounded-lg bg-white shadow-2xl dark:bg-darkBg-primary">
+      <div className="relative flex w-full h-full flex-col rounded-lg bg-white shadow-2xl dark:bg-darkBg-primary">
         <div className="flex items-center justify-between rounded-t-lg bg-lightBg-secondary p-2 dark:bg-darkBg-secondary">
           <h3 className="font-bold text-lightFg-primary dark:text-darkFg-primary">Lousa</h3>
           <Button variant="danger" size="icon" $rounded onClick={onClose}>
