aa30094cc7ed79e216e6af45aa71f2bdf9410579
danrleylfd <danrley.reply+github@gmail.com>
2025-08-09 17:57:48 -0300

    refactor(Markdown): wrap condition in parentheses for clarity

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -23,7 +23,7 @@ const getTweetId = (url) => {
 }
 
 const Markdown = ({ loading, content, think }) => {
-const [collapsed, setCollapsed] = useState(think && !loading)
+const [collapsed, setCollapsed] = useState((think && !loading))
 
   const toggleCollapse = () => setCollapsed((prev) => !prev)
 
