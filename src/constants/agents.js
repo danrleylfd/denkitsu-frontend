8150f36750645b1ce3fde8dab4a260bf82da9788
danrleylfd <danrley.reply+github@gmail.com>
2025-08-17 15:32:32 -0300

    refactor(agents): simplify icon definitions by removing JSX elements

--- src/constants/agents.js
+++ src/constants/agents.js
@@ -1,13 +1,13 @@
 import { Bot, BarChart2, Rss, Code, Presentation, Lightbulb, Newspaper, ListTree, Speech } from "lucide-react"
 
 export const AGENTS_DEFINITIONS = [
-  { value: "Padrão", icon: <Bot size={16} /> },
-  { value: "Analista", icon: <BarChart2 size={16} /> },
-  { value: "Blogueiro", icon: <Rss size={16} /> },
-  { value: "Desenvolvedor", icon: <Code size={16} /> },
-  { value: "Lousa", icon: <Presentation size={16} /> },
-  { value: "Prompter", icon: <Lightbulb size={16} /> },
-  { value: "Redator", icon: <Newspaper size={16} /> },
-  { value: "Secretário", icon: <ListTree size={16} /> },
-  { value: "Transcritor", icon: <Speech size={16} /> },
+  { value: "Padrão", icon: Bot },
+  { value: "Analista", icon: BarChart2 },
+  { value: "Blogueiro", icon: Rss },
+  { value: "Desenvolvedor", icon: Code },
+  { value: "Lousa", icon: Presentation },
+  { value: "Prompter", icon: Lightbulb },
+  { value: "Redator", icon: Newspaper },
+  { value: "Secretário", icon: ListTree },
+  { value: "Transcritor", icon: Speech },
 ]
