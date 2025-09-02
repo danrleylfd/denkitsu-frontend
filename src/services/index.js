9864a952ffd9fe78c7ed801d21f44bff17f22e9e
danrleylfd <danrley.reply+github@gmail.com>
2025-09-02 17:14:50 -0300

    refactor(api): remove request interceptor for origin header
    The interceptor adding Origin header is no longer needed as the backend service has been updated to handle CORS differently.

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,5 @@
 import axios from "axios"
 
 const api = axios.create({ baseURL: "https://denkitsu.up.railway.app" })
-api.interceptors.request.use((config) => {
-  config.headers["Origin"] = "https://denkitsu.vercel.app"
-  return config
-})
 
 export default api
